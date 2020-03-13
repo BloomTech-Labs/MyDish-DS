d3ea8add28a9e165e7d1b9d96b9cb5ec61700763
ToddMG <munyontodd@gmail.com>
2020-03-13 13:28:04 -0700

    Fixed image_extension test

--- Dishify/unit_test.py
+++ Dishify/unit_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-image_uri = 'https://weelicious.com/imager/weelicious_com/wp-content/uploads/2012/09/blueberrypancakes-book-spread_1b74faffbe944b0675f0e20473d3ad34.jpg'
+image_uri = 'https://images-na.ssl-images-amazon.com/images/I/81%2B1uNsyR0L.jpg'
 api_call = f"https://3z6kv0n0v6.execute-api.us-east-2.amazonaws.com/test/dishify?imageURL={image_uri}"
 
 
@@ -17,8 +17,11 @@ class TestImageUrl(unittest.TestCase):
         Checks if the URI ends in an image extension
         """
         image_extensions = ['JPEG', 'JPG', 'PNG', 'PNG8', 'PNG24', 'GIF', 'BMP', 'WEBP', 'RAW', 'ICO', 'PDF', 'TIFF']
-        any_in = lambda image_extensions, image_uri: any(i in image_uri for i in image_extensions)
-        self.assertTrue(any_in)
+        test = False
+        for i in image_extensions:
+            if i.lower() in image_uri:
+                test = True
+        self.assertTrue(test)
 
 
 if __name__ == '__main__':
