a3d3a8241fdfa0069646a98f53e9fb12e9030672
Yoni Pineda <yonipineda1010@icloud.com>
2020-05-12 12:52:33 -0400

    attemp number?

--- Dishify2/app/application.py
+++ Dishify2/app/application.py
@@ -10,7 +10,7 @@ from ingredients_populater.getter import ingredient_getter
 """
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 there is a need to implement exception handling when the picture that was
-taken is that unclear that the google vision api does not return any text 
+taken is that unclear that the google vision api does not return any text
 in texts[0].description
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 """
@@ -19,15 +19,17 @@ in texts[0].description
 class ImageJson(BaseModel):
     image: str
 
+
 class StringJson(BaseModel):
     word: str
 
-application = app = FastAPI()
+
+app = FastAPI()
 
 
 @app.post("/recipe_parser/ingredients/")
 async def create_item(item: ImageJson):
-    
+
     """
     ???????????
     takes in a Json, decodes the string in it to a picture, makes a vision api call
@@ -35,10 +37,9 @@ async def create_item(item: ImageJson):
     """
     item = jsonable_encoder(item)
     image_string = item["image"]
-    texts, blocks = decode_string_and_api_call(image_string) 
+    texts, blocks = decode_string_and_api_call(image_string)
     ingredients = parse_ingredients(texts[0].description)
     return ingredients
-
 
 
 @app.post("/recipe_parser/instructions/")
@@ -50,7 +51,7 @@ async def create_item(item: ImageJson):
     """
     item = jsonable_encoder(item)
     image_string = item["image"]
-    texts, blocks = decode_string_and_api_call(image_string) 
+    texts, blocks = decode_string_and_api_call(image_string)
     instructions = parse_instructions(texts[0].description)
     return instructions
 
@@ -59,10 +60,9 @@ async def create_item(item: ImageJson):
 async def feature(item: StringJson):
     """
     Queries data base from a given name of a recipe, returns ingredients of recipes
-    with matching title where the ingredients occur in more than 25 % of recipes. 
+    with matching title where the ingredients occur in more than 25 % of recipes.
     """
     item = jsonable_encoder(item)
     word_string = item['word']
     results_json = ingredient_getter(word_string)
     return results_json
-
